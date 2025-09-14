2195886b181216b4f4d77cc8b103de9c77deb346
Keith Eberhard <keitheberhard93@gmail.com>
2025-09-13 20:31:32 -0700

    uploading SQL queries.

---
+++ database/assignment2.sql
@@ -0,0 +1,18 @@
+-- Insert of Tony Stark
+INSERT INTO public.account (account_firstname, account_lastname, account_email, account_password)
+	VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+-- Change Tony to admin
+UPDATE public.account SET account_type = 'Admin' WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
+
+-- Delete Tony Stark record from DB
+DELETE FROM public.account WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
+
+-- Modify GM hummer desc
+UPDATE public.inventory SET inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior') WHERE inv_model = 'Hummer';
+
+-- Inner join
+SELECT inv_make, inv_model FROM public.inventory INNER JOIN public.classification ON inventory.classification_id = classification.classification_id WHERE classification.classification_name = 'Sport';
+
+-- Update inventory records
+UPDATE public.inventory SET inv_image = REPLACE(inv_image, '/images', '/images/vehicles'), inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles');
